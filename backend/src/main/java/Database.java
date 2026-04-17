f248befdb5761e0cb08f024e2f194454d29ec771
Michael Carpiano <michael.carpiano@smus.ca>
2026-04-16 18:02:46 -0700

    Removed hardcoded username/password, replaced with env. variables

--- backend/src/main/java/Database.java
+++ backend/src/main/java/Database.java
@@ -7,17 +7,15 @@ public class Database {
     private static final String URL =
             "jdbc:postgresql://aws-1-us-east-1.pooler.supabase.com:5432/postgres?sslmode=require";
 
-    private static final String USER = "postgres.esmodnfwtcrhsldqysxf";
-    private static final String PASSWORD = "APCSA2026?!";
+    private static final String un = System.getenv("USERNAME");
+    private static final String pw = System.getenv("PASSWORD");
 
-    public static Connection getConnection() {
-        try {
-            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connected!");
-            return conn;
+    public static void getConnection() {
+        try (Connection conn = DriverManager.getConnection(URL, un, pw)) {
+            System.out.println("Connected successfully!");
         } catch (SQLException e) {
+            System.out.println("Connection failed:");
             e.printStackTrace();
-            return null;
         }
     }
 }
